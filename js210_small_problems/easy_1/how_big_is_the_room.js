0e326caf8124a9afc16c4ea844e78c9e33d277f9
Stephen Johnston <sdjohnston1990@gmail.com>
2021-02-01 14:25:44 -0400

    Add exercise file from Easy 1

---
+++ js210_small_problems/easy_1/how_big_is_the_room.js
@@ -0,0 +1,55 @@
+// JS210 - Small Problems > Easy 1
+
+/*
+How Big is the Room
+-------------------
+
+Build a program that asks the user to enter the length and width of a room in meters, 
+and then logs the area of the room to the console in both square meters and square feet.
+
+Note: 1 square meter == 10.7639 square feet
+
+Do not worry about validating the input at this time. Use the readlineSync.prompt method 
+to collect user input.
+
+Example:
+--------
+
+Enter the length of the room in meters:
+10
+Enter the width of the room in meters:
+7
+The area of the room is 70.00 square meters (753.47 square feet).
+*/
+
+let readlineSync = require('readline-sync');
+
+const SQMETERS_TO_SQFEET = 10.7639;
+
+let length = readlineSync.question('Enter the length of the room in meters: ');
+let width = readlineSync.question('Enter the width of the room in meters: ');
+
+let areaInMeters = length * width;
+let areaInFeet = areaInMeters * SQMETERS_TO_SQFEET
+console.log(`The area of the room is ${areaInMeters.toFixed(2)} square meters (${areaInFeet.toFixed(2)} square feet)`);
+
+// LS Solution:
+
+let readlineSync = require("readline-sync");
+
+const SQMETERS_TO_SQFEET = 10.7639;
+
+console.log("Enter the length of the room in meters:");
+let length = readlineSync.prompt();
+length = parseInt(length, 10);
+
+console.log("Enter the width of the room in meters:");
+let width = readlineSync.prompt();
+width = parseInt(width, 10);
+
+let areaInMeters = (length * width);
+let areaInFeet = (areaInMeters * SQMETERS_TO_SQFEET);
+
+console.log(
+  `The area of the room is ${areaInMeters.toFixed(2)} square meters (${areaInFeet.toFixed(2)} square feet).`
+);
