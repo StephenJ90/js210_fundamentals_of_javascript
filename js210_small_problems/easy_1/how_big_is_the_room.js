1dcdb1b384e9476d5f8c67b66cd09a8e44dd5146
Stephen Johnston <sdjohnston1990@gmail.com>
2021-02-03 12:36:39 -0400

    Add revised exercise file from easy 1

--- js210_small_problems/easy_1/how_big_is_the_room.js
+++ js210_small_problems/easy_1/how_big_is_the_room.js
@@ -22,34 +22,74 @@ Enter the width of the room in meters:
 The area of the room is 70.00 square meters (753.47 square feet).
 */
 
-let readlineSync = require('readline-sync');
+// let readlineSync = require('readline-sync');
 
-const SQMETERS_TO_SQFEET = 10.7639;
+// const SQMETERS_TO_SQFEET = 10.7639;
 
-let length = readlineSync.question('Enter the length of the room in meters: ');
-let width = readlineSync.question('Enter the width of the room in meters: ');
+// let length = readlineSync.question('Enter the length of the room in meters: ');
+// let width = readlineSync.question('Enter the width of the room in meters: ');
 
-let areaInMeters = length * width;
-let areaInFeet = areaInMeters * SQMETERS_TO_SQFEET
-console.log(`The area of the room is ${areaInMeters.toFixed(2)} square meters (${areaInFeet.toFixed(2)} square feet)`);
+// let areaInMeters = length * width;
+// let areaInFeet = areaInMeters * SQMETERS_TO_SQFEET
+// console.log(`The area of the room is ${areaInMeters.toFixed(2)} square meters (${areaInFeet.toFixed(2)} square feet)`);
 
 // LS Solution:
+
+// let readlineSync = require("readline-sync");
+
+// const SQMETERS_TO_SQFEET = 10.7639;
+
+// console.log("Enter the length of the room in meters:");
+// let length = readlineSync.prompt();
+// length = parseInt(length, 10);
+
+// console.log("Enter the width of the room in meters:");
+// let width = readlineSync.prompt();
+// width = parseInt(width, 10);
+
+// let areaInMeters = (length * width);
+// let areaInFeet = (areaInMeters * SQMETERS_TO_SQFEET);
+
+// console.log(
+//   `The area of the room is ${areaInMeters.toFixed(2)} square meters (${areaInFeet.toFixed(2)} square feet).`
+// );
+
+// Further Exploration:
+// Modify the program so that it asks the user for the input type (meters or feet). Compute for 
+// the area accordingly, and log it and its conversion in parentheses.
+// I'll modify LS's Solution:
 
 let readlineSync = require("readline-sync");
 
 const SQMETERS_TO_SQFEET = 10.7639;
+const SQFEET_TO_SQMETERS = 0.092903;
 
-console.log("Enter the length of the room in meters:");
+console.log('Enter Unit of Measurement (meters or feet):')
+let unitType = readlineSync.prompt();
+
+if (unitType !== 'meters' && unitType !== 'feet') {
+  console.log('Error: Invalid Unit Type');
+  return;
+}
+
+console.log(`Enter the length of the room in ${unitType}: `);
 let length = readlineSync.prompt();
 length = parseInt(length, 10);
 
-console.log("Enter the width of the room in meters:");
+console.log(`Enter the width of the room in ${unitType}: `);
 let width = readlineSync.prompt();
 width = parseInt(width, 10);
 
-let areaInMeters = (length * width);
-let areaInFeet = (areaInMeters * SQMETERS_TO_SQFEET);
+let area = (length * width);
+let areaInFeet = area * SQMETERS_TO_SQFEET
+let areaInMeters = area * SQFEET_TO_SQMETERS
 
-console.log(
-  `The area of the room is ${areaInMeters.toFixed(2)} square meters (${areaInFeet.toFixed(2)} square feet).`
-);
+if (unitType === 'meters') {
+  console.log(
+  `The area of the room is ${area.toFixed(2)} square meters (${areaInFeet.toFixed(2)} square feet).`
+  );
+} else {
+  console.log(
+  `The area of the room is ${area.toFixed(2)} square feet (${areaInMeters.toFixed(2)} square meters).`
+  );
+}
