9a03d52b36bf348f1f664662774cd4d41e1cd240
Stephen Johnston <sdjohnston1990@gmail.com>
2021-02-21 13:17:09 -0400

    Add file from lesson 4

---
+++ js210_lesson_4/practice_problems_arrays/comparing_arrays.js
@@ -0,0 +1,44 @@
+// JS210 Fundamentals of JavaScript for Programmers > Arrays > Practice Problem: Comparing Arrays
+
+/*
+Comparing Arrays
+----------------
+
+Write a function named arraysEqual that takes two arrays as arguments. The function 
+should return true if the arrays contain the same values, or false if they do not.
+
+Test the function with arrays that contain number, string, and boolean values. Don't 
+worry about handling arrays that contain other Arrays or Objects.
+
+Examples:
+
+arraysEqual([1], [1]);                               // true
+arraysEqual([1], [2]);                               // false
+arraysEqual([1, 2], [1, 2, 3]);                      // false
+arraysEqual([1, 'hi', true], [1, 'hi', true]);       // true
+arraysEqual([1, 'hi', true], [1, 'hi', false]);      // false
+arraysEqual([1, 'hi', true], [1, 'hello', true]);    // false
+arraysEqual([1, 'hi', true], [2, 'hi', true]);       // false
+*/
+
+function arraysEqual(left, right) {
+  if (left.length !== right.length) {
+    return false;
+  }
+
+  for (let index = 0; index < left.length; index += 1) {
+    if (left[index] !== right[index]) {
+      return false
+    }
+  }
+
+  return true;
+}
+
+arraysEqual([1], [1]);                               // true
+arraysEqual([1], [2]);                               // false
+arraysEqual([1, 2], [1, 2, 3]);                      // false
+arraysEqual([1, 'hi', true], [1, 'hi', true]);       // true
+arraysEqual([1, 'hi', true], [1, 'hi', false]);      // false
+arraysEqual([1, 'hi', true], [1, 'hello', true]);    // false
+arraysEqual([1, 'hi', true], [2, 'hi', true]);       // false
