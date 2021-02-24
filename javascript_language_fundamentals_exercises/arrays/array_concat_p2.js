32b9ec324b09177e3d6d05fa59e7d66ef0da1a4f
Stephen Johnston <sdjohnston1990@gmail.com>
2021-02-23 20:05:32 -0400

    Add concat method part 2

--- javascript_language_fundamentals_exercises/arrays/array_concat_p2.js
+++ javascript_language_fundamentals_exercises/arrays/array_concat_p2.js
@@ -36,3 +36,9 @@ function concat(...args) {
 concat([1, 2, 3], [4, 5, 6], [7, 8, 9]);    // [1, 2, 3, 4, 5, 6, 7, 8, 9]
 concat([1, 2], 'a', ['one', 'two']);        // [1, 2, "a", "one", "two"]
 concat([1, 2], ['three'], 4);               // [1, 2, "three", 4]
+
+// Student Solution:
+
+function concat(...args) {
+  return Array.from(args).flat();
+}
